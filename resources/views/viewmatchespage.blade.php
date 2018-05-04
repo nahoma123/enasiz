90237255b50217160f6f7c5cd0437c61fb50c219
Nahom Asnake <nahomasnake12@gmai.com>
2018-05-04 15:20:32 +0400

    after merge with yared todo:notification

--- resources/views/viewmatchespage.blade.php
+++ resources/views/viewmatchespage.blade.php
@@ -9,11 +9,8 @@
             <div class="row">
 
 
-<<<<<<< HEAD
                 <div class="col-md-7">
-=======
-                <div class="col-md-9">
->>>>>>> 299c5157c1631db250a3311a01b4e55a92e3230c
+                <div class="col-md-12">
                     <div class="table-responsive">
 
                         <table id="mytable" class="table table-bordred table-striped">
@@ -30,18 +27,13 @@
                             @foreach($matches as $match)
                                 <tr>
                                     <td>{{ $match->competition->league_name }}</td>
-<<<<<<< HEAD
+
                                     <td><h6>{{$match->hometeam[0]->team_thumbnail}}</h6><img src="/storage/upload/team_thumbnail/Capture.JPG" style="width: 40px; height: 40px; border-radius: 50%;">{{ $match->hometeam[0]->team_name }} vs {{ $match->awayteam[0]->team_name }}</td>
                                     <td>{{ $match->start_time }}</td>
-                                    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                                    <td><a href="/updateMatch/{{$match->id}}"><button class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-pencil"></span></button></a></td>
                                     <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
                                     <td><button><a href="/addBetOnMatch/{{$match->id}}">Add bet</a></button></td>
-=======
-                                    <td>{{ $match->hometeam[0]->team_name }} vs {{ $match->awayteam[0]->team_name }}</td>
-                                    <td>{{ $match->start_time }}</td>
-                                    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-                                    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
->>>>>>> 299c5157c1631db250a3311a01b4e55a92e3230c
+
                                 </tr>
                             @endforeach
                             </tbody>
@@ -61,39 +53,6 @@
             </div>
         </div>
 
-
-        <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-                        <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <input class="form-control " type="text" placeholder="Mohsin">
-                        </div>
-                        <div class="form-group">
-
-                            <input class="form-control " type="text" placeholder="Irshad">
-                        </div>
-                        <div class="form-group">
-                            <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
-
-
-                        </div>
-                    </div>
-                    <div class="modal-footer ">
-                        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
-                    </div>
-                </div>
-                <!-- /.modal-content -->
-            </div>
-            <!-- /.modal-dialog -->
-        </div>
-
-
-
         <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -107,12 +66,12 @@
 
                     </div>
                     <div class="modal-footer ">
-<<<<<<< HEAD
+
                         <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
-=======
+
                         <a href="deleteMatch/{{$match->id}}"><button  type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button></a>
                         
->>>>>>> 299c5157c1631db250a3311a01b4e55a92e3230c
+
                         <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
                     </div>
                 </div>
